9bff52a7b6ac735738d3b760f78f1517403be54e
NazariiPrubniak <72340709+NazariiPrubniak@users.noreply.github.com>
2023-06-27 13:01:28 +0300

    Add files via upload

---
+++ Data/AccountContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wrap.Data
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<Wrap.Model.Account>? Account { get; set; }
+    }
+}
